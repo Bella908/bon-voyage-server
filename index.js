040b6df865c61f7ddc2a892b3addeadc3428d558
bella908 <mumtahina efa8@gmail.com>
2024-04-30 01:18:04 +0600

    update delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const cors = require ('cors');
+const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,49 +28,58 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-const spotCollection =client.db('bonVoyageDB').collection('spot');
+    const spotCollection = client.db('bonVoyageDB').collection('spot');
 
-// show data in server
- app.get('/newSpot',async(req ,res)=>{
-  const cursor = spotCollection.find();
-  const result = await cursor.toArray();
-  res.send(result);
- })
-
-
-
-app.post('/newSpot',async(req,res)=>{
-  const newSpot = req.body;
-  console.log(newSpot);
-  const result = await spotCollection.insertOne(newSpot);
-  res.send(result);
- })
-
-app.get("/showList/:email" ,async(req,res)=>{
-  // const newSpot = req.body;
-  console.log(req.params.email);
-  const result = await spotCollection.find({email:req.params.email}).toArray();
-  // const result = await spotCollection.insertOne(newSpot);
-   res.send(result);
- })
+    // show data in server
+    app.get('/newSpot', async (req, res) => {
+      const cursor = spotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
 
- app.get("/viewDetails/:_id" ,async(req,res)=>{
-   console.log(req.params._id);
-  const result = await spotCollection.findOne({_id:new ObjectId( req.params._id)});
- console.log(result)
- res.send(result)
- })
+
+    app.post('/newSpot', async (req, res) => {
+      const newSpot = req.body;
+      console.log(newSpot);
+      const result = await spotCollection.insertOne(newSpot);
+      res.send(result);
+    })
+
+    app.get("/showList/:email", async (req, res) => {
+      // const newSpot = req.body;
+      console.log(req.params.email);
+      const result = await spotCollection.find({ email: req.params.email }).toArray();
+      // const result = await spotCollection.insertOne(newSpot);
+      res.send(result);
+    })
 
 
- app.delete("/showList/delete/:_id", async(req,res)=>{
-  
- const result = await spotCollection.deleteOne({_id:new ObjectId( req.params._id)});
-console.log(result)
-res.send(result)
-})
+    app.get("/viewDetails/:_id", async (req, res) => {
+      console.log(req.params._id);
+      const result = await spotCollection.findOne({ _id: new ObjectId(req.params._id) });
+      console.log(result)
+      res.send(result)
+    })
+
+
+    app.delete("/showList/delete/:_id", async (req, res) => {
+      const result = await spotCollection.deleteOne({ _id: new ObjectId(req.params._id) });
+      console.log(result)
+      res.send(result)
+    })
+
+
+    app.delete("/showList/update/:_id", async (req, res) => {
+      const id = req.params.id;
+      const result = await spotCollection.deleteOne({ _id: new ObjectId(id) });
+      console.log(result)
+      res.send(result)
+    })
 
     
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -83,11 +92,11 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req ,res) =>{
-    res.send('bon voyge is running')
+app.get('/', (req, res) => {
+  res.send('bon voyge is running')
 
 })
 
-app.listen(port,() =>{
-    console.log(`bon voyge is running on port:${port}`)
+app.listen(port, () => {
+  console.log(`bon voyge is running on port:${port}`)
 })
